34e9199c42b986b4157d639c5303c771ea15661f
Basix <jsrtyu1120@gmail.com>
2016-07-09 23:52:26 +0900

    편지 쓰는 프로그램 만들기

--- renderer.js
+++ renderer.js
@@ -1,3 +1,7 @@
-// This file is required by the index.html file and will
-// be executed in the renderer process for that window.
-// All of the Node.js APIs are available in this process.
+var fs = require('fs'); //일단 파일 모듈부터 부른다.
+$('.class1 button#btn').on('click', function () { // 버튼 1 클릭시
+  fs.writeFile('letter.txt', `안녕, ${$('.class1 input#name').prop('value')}. 편지 내용은 이게 다야!`, (err) => { // 파일 쓰기
+    if (err) throw err; // 에러는 대충 처리한다
+    $('.class1').append('<p>편지 쓰기 끝!</p>'); // 편지 쓰기 끝 이라고 써 놓는다.
+  });
+})
